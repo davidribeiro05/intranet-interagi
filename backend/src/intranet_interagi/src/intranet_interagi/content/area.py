e04e5fe260616ca9d6f8c5d5f3d599b5aa89ee57
David Ribeiro <david.ribeiro05@hotmail.com>
2023-05-13 10:25:32 -0300

    Exercicio 26: Remover duplicação de código

--- backend/src/intranet_interagi/src/intranet_interagi/content/area.py
+++ backend/src/intranet_interagi/src/intranet_interagi/content/area.py
@@ -1,22 +1,14 @@
 from intranet_interagi import _
+from intranet_interagi.validadores import do_validation
+from intranet_interagi.validadores import is_valid_email
+from intranet_interagi.validadores import is_valid_ramal
 from plone.dexterity.content import Container
 from plone.schema.email import Email
 from plone.supermodel import model
 from plone.supermodel.model import Schema
 from zope import schema
 from zope.interface import implementer
-
-import re
-
-
-def is_valid_ramal(value: str) -> bool:
-    """Validar se o ramal tem 4 dígitos númericos."""
-    return re.match(r"^\d{4}$", value) if value else True
-
-
-def is_valid_email(value: str) -> bool:
-    """Validar se o email é @plone.org."""
-    return value.endswith("@plone.org") if value else True
+from zope.interface import invariant
 
 
 class IArea(Schema):
@@ -31,6 +23,11 @@ class IArea(Schema):
     # Basic info
     ramal = schema.TextLine(title=_("Ramal"), required=False, constraint=is_valid_ramal)
     email = Email(title=_("Email"), required=False, constraint=is_valid_email)
+
+    @invariant
+    def validate_email(data):
+        """Validate email set by the user."""
+        do_validation(data)
 
 
 @implementer(IArea)
