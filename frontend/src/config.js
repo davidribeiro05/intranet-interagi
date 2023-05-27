e344ef41745e2902df0001765418bd81fe24759d
David Ribeiro <david.ribeiro05@hotmail.com>
2023-05-27 19:13:52 -0300

    Exercício 50: criar view para Pessoa

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -13,6 +13,8 @@
  * }
  */
 import AreaView from './components/View/AreaView';
+import PessoaView from './components/View/PessoaView';
+
 // All your imports required for the config here BEFORE this line
 import '@plone/volto/config';
 
@@ -27,6 +29,7 @@ export default function applyConfig(config) {
   config.views.contentTypesViews = {
     ...config.views.contentTypesViews,
     Area: AreaView,
+    Pessoa: PessoaView,
   };
   return config;
 }
