45505217f6456263a090e1a7f89c8b097ff1d896
David Ribeiro <david.ribeiro05@hotmail.com>
2023-05-27 22:30:32 -0300

    Exercício 56: adicionando índice Predio

--- backend/src/intranet_interagi/src/intranet_interagi/upgrades/catalog.py
+++ backend/src/intranet_interagi/src/intranet_interagi/upgrades/catalog.py
@@ -12,3 +12,15 @@ def reindexa_pessoa(context):
         logger.info(f"- Reindexa o campo area do objeto {pessoa.absolute_url()}")
 
     logger.info("Reindexação completa")
+
+
+def reindexa_predio(context):
+    # Reindexa o campo predio do tipo pessoa
+    brains = api.content.find(portal_type=["Area", "Pessoa"])
+
+    for brain in brains:
+        conteudo = brain.getObject()
+        conteudo.reindexObject(idxs=["predio"])
+        logger.info(f"- Reindexa o campo predio do objeto {conteudo.absolute_url()}")
+
+    logger.info("Reindexação completa")
